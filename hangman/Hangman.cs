04a098b64517543a377ae437577e282d3aa613f4
jame2408 <jame2408@gmail.com>
2017-02-05 16:18:03 +0800

    Initial project and add hangman length.

---
+++ hangman/Hangman.cs
@@ -0,0 +1,17 @@
+﻿namespace hangman
+{
+    public class Hangman
+    {
+        private string _word;
+
+        public Hangman(string word)
+        {
+            this._word = word;
+        }
+
+        public int length()
+        {
+            return _word.Length;
+        }
+    }
+}
---
+++ hangmanTests/HangmanLengthTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hangman.Tests
+{
+    [TestClass()]
+    public class HangmanLengthTests
+    {
+        [TestMethod()]
+        public void Length_of_word()
+        {
+            var hangman = new Hangman("word");
+
+            Assert.AreEqual(4, hangman.length());
+        }
+    }
+}
